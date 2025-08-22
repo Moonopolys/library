fb444fec08550d448fb38642cb86816098cf5d64
Moonopolys <sobirova1202@gmail.com>
2025-08-22 05:47:45 +0500

    inex.html yozildi

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-
+    path('', home, name='home'),
+    path('genre/<int:genre_id>/', books_by_genres, name = 'by_genre'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
